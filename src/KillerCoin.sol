9294b70fc3d25116fd500eae360d3e9f55b86131
sohamkumar <sohamkumar17072003srp@gmail.com>
2024-08-15 01:35:35 +0530

    Redeeming collateral and burning the killer coin function initialized anf frmatted the code

--- src/KillerCoin.sol
+++ src/KillerCoin.sol
@@ -5,28 +5,28 @@ pragma solidity ^0.8.26;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract KillerCoin is ERC20Burnable, Ownable{
+contract KillerCoin is ERC20Burnable, Ownable {
     error KillerCoin__InValidAddress();
     error KillerCoin__InValidAmount();
 
-    constructor() Ownable(msg.sender) ERC20("KillerCoin","KILL"){}
+    constructor() Ownable(msg.sender) ERC20("KillerCoin", "KILL") {}
 
-    function mint(address _to, uint256 _amount) public onlyOwner returns(bool){
-        if(_to == address(0)){
+    function mint(address _to, uint256 _amount) public onlyOwner returns (bool) {
+        if (_to == address(0)) {
             revert KillerCoin__InValidAddress();
         }
-        if(_amount == 0){
+        if (_amount == 0) {
             revert KillerCoin__InValidAmount();
         }
-        _mint(_to,_amount);
+        _mint(_to, _amount);
         return true;
     }
 
-    function burn(uint256 _amount) public onlyOwner override{
-        if(msg.sender == address(0)){
+    function burn(uint256 _amount) public override onlyOwner {
+        if (msg.sender == address(0)) {
             revert KillerCoin__InValidAddress();
         }
-        if(_amount == 0){
+        if (_amount == 0) {
             revert KillerCoin__InValidAmount();
         }
         super.burn(_amount);
--- src/KillerDSCEngine.sol
+++ src/KillerDSCEngine.sol
@@ -19,11 +19,6 @@
 // private
 // view & pure functions
 
-
-
-
-
-
 //SPDX-License-Identifier:MIT
 
 pragma solidity ^0.8.26;
@@ -38,21 +33,16 @@ import {KillerCoin} from "./KillerCoin.sol";
  *         pegged to 1USD, algorihmically stable and WETH and WBTC are the two collateral
  *         token type which is accepted as collataral
  */
-
 contract KillerDSCEngine {
-
-
     /////////////////////////////
     ////    State Variables  ////
     /////////////////////////////
-    
+
     error KillerDSCEngine__TokensArrayLengthAndPricefeedsArrayLengthUnEqual();
     error KillerDSCEngine__TokenNotAllowedAsColletaral(address tokenAdd);
     error KillerDSCEngine__InvalidAmount();
     error KillerDSCEngine__InvalidUSER();
-    error KillerDSCEngine__FailedDepositingCollateral();
-    error KillerDSCEngine__FailedMinitingKillerCoin();
-
+    error KillerDSCEngine__TransferedFailed();
 
     /////////////////////////////
     ////    State Variables  ////
@@ -62,91 +52,138 @@ contract KillerDSCEngine {
     mapping(address tokenAdd => address pricefeedAdd) private s_ValidCollateralTokens;
     mapping(address user => uint256 amount) private s_killerMinted;
     mapping(address user => mapping(address tokenAdd => uint256 amount)) private s_depositedCollateral;
-    
+
     /////////////////////////////
     ////     Events          ////
     /////////////////////////////
 
     event CollateralDeposited(address indexed user, address indexed tokenCollateralAdd, uint256 amount);
-
+    event KillerCoinMinted(address indexed user, uint256 amount);
+    event CollateralRedeemed(address indexed user, address indexed tokenCollateralAdd, uint256 amount);
+    event KillerCoinBurned(address indexed user, uint256 amount);
 
     /////////////////////////////
     ////  Modifiers          ////
     /////////////////////////////
 
     modifier ValidCollateral(address tokenAdd) {
-        if(s_ValidCollateralTokens[tokenAdd] == address(0)){
+        if (s_ValidCollateralTokens[tokenAdd] == address(0)) {
             revert KillerDSCEngine__TokenNotAllowedAsColletaral(tokenAdd);
         }
         _;
     }
 
     modifier ValidAmount(uint256 amount) {
-        if(amount == 0){
+        if (amount == 0) {
             revert KillerDSCEngine__InvalidAmount();
         }
         _;
     }
-
 
     /////////////////////////////
     ////    Functions        ////
     /////////////////////////////
 
     constructor(address[] memory validTokenAdd, address[] memory pricefeedAddes) {
-        if(validTokenAdd.length != pricefeedAddes.length){
+        if (validTokenAdd.length != pricefeedAddes.length) {
             revert KillerDSCEngine__TokensArrayLengthAndPricefeedsArrayLengthUnEqual();
         }
-        for(uint256 i = 0; i < validTokenAdd.length; i++){
+        for (uint256 i = 0; i < validTokenAdd.length; i++) {
             s_ValidCollateralTokens[validTokenAdd[i]] = pricefeedAddes[i];
         }
 
         i_killer = new KillerCoin();
     }
 
-    function depositCollateralAndMintKiller(address tokenCollateralAdd, uint256 collateralAmount, uint256 killerToMint) public {}
+    function depositCollateralAndMintKiller(address tokenCollateralAdd, uint256 collateralAmount, uint256 killerToMint)
+        public
+    {
+        depositCollateral(tokenCollateralAdd, collateralAmount);
+        mintKiller(killerToMint);
+    }
 
-    function depositCollateral(address tokenCollateralAdd, uint256 collateralAmount) public{
-
-        if(msg.sender == address(0)){
+    function depositCollateral(address tokenCollateralAdd, uint256 collateralAmount) public {
+        if (msg.sender == address(0)) {
             revert KillerDSCEngine__InvalidUSER();
         }
-        _depositCollateral(msg.sender,tokenCollateralAdd,collateralAmount);
+        _depositCollateral(msg.sender, tokenCollateralAdd, collateralAmount);
     }
 
     function mintKiller(uint256 killerToMint) public ValidAmount(killerToMint) {
-        //check amount
-        //users[msg.sender] += amount
-        //revert if health factor is broken   create sepreate function for calculate healthfactor
-        //mint killer in killercoin contract instance
-        //done
+        if (msg.sender == address(0)) {
+            revert KillerDSCEngine__InvalidUSER();
+        }
+        _mintKiller(msg.sender, killerToMint);
     }
-    
-    function redeemCollateralAndBurnKiller(address tokenCollateralAdd, uint256 collateralAmount, uint256 killerToBurn) public {}
 
-    function redeemCollateral(address tokenCollateralAdd, uint256 collateralAmount) public {}
+    function redeemCollateralAndBurnKiller(address tokenCollateralAdd, uint256 collateralAmount, uint256 killerToBurn)
+        public
+    {}
 
-    function BurnKiller(uint256 killerToBurn) public {}
+    function redeemCollateral(address tokenCollateralAdd, uint256 collateralAmount) public {
+        if (msg.sender == address(0)) {
+            revert KillerDSCEngine__InvalidUSER();
+        }
+        _redeemCollateral(msg.sender, tokenCollateralAdd, collateralAmount);
+    }
 
-    function _depositCollateral(address _to, address _tokenCollateral, uint256 _amount) internal ValidCollateral(_tokenCollateral) ValidAmount(_amount) {
+    function burnKiller(uint256 killerToBurn) public {
+        if (msg.sender == address(0)) {
+            revert KillerDSCEngine__InvalidUSER();
+        }
+        _burnKiller(msg.sender, killerToBurn);
+    }
+
+    function _depositCollateral(address _to, address _tokenCollateral, uint256 _amount)
+        internal
+        ValidCollateral(_tokenCollateral)
+        ValidAmount(_amount)
+    {
         s_depositedCollateral[_to][_tokenCollateral] += _amount;
-        emit CollateralDeposited(_to,_tokenCollateral,_amount);
+        emit CollateralDeposited(_to, _tokenCollateral, _amount);
 
-        bool success = IERC20(_tokenCollateral).transferFrom(_to,address(this),_amount);
+        bool success = IERC20(_tokenCollateral).transferFrom(_to, address(this), _amount);
 
-        if(!success){
-            revert KillerDSCEngine__FailedDepositingCollateral();
+        if (!success) {
+            revert KillerDSCEngine__TransferedFailed();
         }
     }
 
     function _mintKiller(address _to, uint256 _amount) internal ValidAmount(_amount) {
         s_killerMinted[_to] += _amount;
         _revertIfHealthFactorIsBroken(_to);
+        emit KillerCoinMinted(_to, _amount);
 
-        bool isMinted = i_killer.mint(_to,_amount);
-        if(!isMinted){
-            revert KillerDSCEngine__FailedMinitingKillerCoin();
-        }        
+        bool isMinted = i_killer.mint(_to, _amount);
+        if (!isMinted) {
+            revert KillerDSCEngine__TransferedFailed();
+        }
+    }
+
+    function _redeemCollateral(address _from, address _tokenCollateral, uint256 _amount)
+        internal
+        ValidCollateral(_tokenCollateral)
+        ValidAmount(_amount)
+    {
+        s_depositedCollateral[_from][_tokenCollateral] -= _amount;
+        _revertIfHealthFactorIsBroken(_from);
+        emit CollateralRedeemed(_from, _tokenCollateral, _amount);
+
+        bool success = IERC20(_tokenCollateral).transfer(_from, _amount);
+        if (!success) {
+            revert KillerDSCEngine__TransferedFailed();
+        }
+    }
+
+    function _burnKiller(address _from, uint256 _amount) internal ValidAmount(_amount) {
+        s_killerMinted[_from] -= _amount;
+        emit KillerCoinBurned(_from, _amount);
+
+        bool success = i_killer.transferFrom(_from, address(this), _amount);
+        if (!success) {
+            revert KillerDSCEngine__TransferedFailed();
+        }
+        i_killer.burn(_amount);
     }
 
     function _revertIfHealthFactorIsBroken(address _to) internal {}
